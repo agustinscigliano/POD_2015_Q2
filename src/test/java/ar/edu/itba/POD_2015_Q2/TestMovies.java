9ba2bb379d626beee633f0e87b556603d4b73924
Julian Gutierrez Ferrara <jugumac@lab208.it.itba.edu.ar>
2015-11-16 14:33:48 -0300

    prep for demo

--- src/test/java/ar/edu/itba/POD_2015_Q2/TestMovies.java
+++ src/test/java/ar/edu/itba/POD_2015_Q2/TestMovies.java
@@ -9,16 +9,16 @@ import ar.edu.itba.model.MovieLoader;
 public class TestMovies extends TestCase {
 	public void testMovie() 		
 	{	
-		try {
-			Movie[] movie =  MovieLoader.loadMovies("res/imdb-20k.json");
-			System.out.println(movie.length);
-			System.out.println(movie[0]);
-			for (Movie movie2 : movie) {
-				System.out.println(movie2.getImdbVotes());
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			Movie[] movie =  MovieLoader.loadMovies("res/imdb-20k.json");
+//			System.out.println(movie.length);
+//			System.out.println(movie[0]);
+//			for (Movie movie2 : movie) {
+//				System.out.println(movie2.getImdbVotes());
+//			}
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 }
