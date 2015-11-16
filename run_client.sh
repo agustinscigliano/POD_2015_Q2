c0ad5668eedc42b1aba5a94b46b81fba1b9c7a49
Julian Gutierrez Ferrara <jugumac@Julians-MacBook-Pro-2.local>
2015-11-16 06:24:41 -0300

    client script fix

--- run_client.sh
+++ run_client.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export JAVA_HOME=$(/usr/libexec/java_home)
 mvn package
+export CLASSPATH=target/classes/:target/POD_2015_Q2-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 java -jar target/POD_2015_Q2-0.0.1-SNAPSHOT-jar-with-dependencies.jar
